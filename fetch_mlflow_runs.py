d8d8ea25c0151c4ec6a858895f1e945b384c5c83
Alex Spanos <ge02040@hotmail.com>
2020-08-26 21:27:17 +0100

    fetch runs

--- fetch_mlflow_runs.py
+++ fetch_mlflow_runs.py
@@ -1,4 +1,5 @@
 import mlflow
+import click
 
 TRACKING_URI = "http://testuser:test@ec2-3-9-174-162.eu-west-2.compute.amazonaws.com"
 
@@ -18,17 +19,18 @@ def fetch_runs(tracking_uri, experiment_id, baseline_query, candidate_query):
     return baseline_run, candidate_run
 
 
-def render_markdown(baseline_run, candidate_run):
-    # header_row = [ms.bold('Model'), ms.bold('Accuracy')]
-    # baseline_row = [ms.italics('Baseline'), baseline_run['metrics.training accuracy']]
-    # candidate_row = [ms.italics('Candidate'), candidate_run['metrics.training accuracy']]
-    # output_table = ms.table_from_rows([header_row, baseline_row])
-    output_table = f"| **Model** | **Accuracy** |\n| ---------- | -------------- |\n| _Baseline_ | {round(baseline_run['metrics.training accuracy'], 4)} |\n| _Candidate_ | {round(candidate_run['metrics.training accuracy'], 4)} |"
-    print(output_table)
-
-
-if __name__ == "__main__":
+@click.command()
+@click.option("--run")
+@click.option("--attribute")
+def emit_variable(run, attribute):
     baseline_run, candidate_run = fetch_runs(
         TRACKING_URI, experiment_id, baseline_query, candidate_query
     )
-    render_markdown(baseline_run, candidate_run)
+    if run == "baseline":
+        click.echo(f"{baseline_run[attribute]}")
+    else:
+        click.echo(f"{candidate_run[attribute]}")
+
+
+if __name__ == "__main__":
+    emit_variable()
