3adfc0f722bb48f973637b46c5c9fee99f687036
Alex Spanos <ge02040@hotmail.com>
2020-08-27 18:08:55 +0100

    fixed fetch live model

---
+++ fetch_currently_deployed_model.py
@@ -0,0 +1,18 @@
+import mlflow
+
+TRACKING_URI = "http://testuser:test@ec2-3-9-174-162.eu-west-2.compute.amazonaws.com"
+
+query = "tags.live='1'"
+experiment_id = "0"
+
+
+def fetch_live_model_id(tracking_uri, experiment_id, query):
+    mlflow.set_tracking_uri(tracking_uri)
+    runs = mlflow.search_runs(experiment_ids=[experiment_id], filter_string=query).iloc[
+        0
+    ]
+    print(runs["run_id"])
+
+
+if __name__ == "__main__":
+    fetch_live_model_id(TRACKING_URI, experiment_id, query)
