4ef0fad37cd75dd8ea2d24f78d34b506e968a153
Alex Spanos <ge02040@hotmail.com>
2020-08-27 18:03:32 +0100

    used the right mlflow tracking syntax

--- overwrite_tags.py
+++ overwrite_tags.py
@@ -8,10 +8,10 @@ TRACKING_URI = "http://testuser:test@ec2-3-9-174-162.eu-west-2.compute.amazonaws
 @click.option("--baseline_run_id")
 @click.option("--candidate_run_id")
 def overwrite_tags(baseline_run_id, candidate_run_id):
-    mlflow.set_tracking_uri(TRACKING_URI)
-    mlflow.tracking.set_tag(baseline_run_id, "live", 0)
-    mlflow.tracking.set_tag(candidate_run_id, "live", 1)
-    mlflow.tracking.set_tag(candidate_run_id, "production_candidate", 0)
+    client = mlflow.tracking.MlflowClient(tracking_uri=TRACKING_URI)
+    client.set_tag(baseline_run_id, "live", 0)
+    client.set_tag(candidate_run_id, "live", 1)
+    client.set_tag(candidate_run_id, "production_candidate", 0)
 
 
 if __name__ == "__main__":
