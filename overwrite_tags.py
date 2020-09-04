80e8324e6af05bd47d7372c82cdaca2a4417d804
Alex Spanos <ge02040@hotmail.com>
2020-09-04 09:14:22 +0100

    cleaning candidate tag

--- overwrite_tags.py
+++ overwrite_tags.py
@@ -15,6 +15,10 @@ def overwrite_tags(baseline_run_id, candidate_run_id):
     client.set_tag(baseline_run_id, "live", 0)
     print(f"Setting live tag to 1 for candidate model run ID {candidate_run_id}")
     client.set_tag(candidate_run_id, "live", 1)
+    print(
+        f"Setting production_candidate tag to 0 for candidate model run ID {candidate_run_id}"
+    )
+    client.set_tag(candidate_run_id, "production_candidate", 0)
     # runs = client.search_runs(experiment_ids=[experiment_id], filter_string=query)
     # if len(runs) > 0:
     #     print("Cleaning up residual production_candidate tags within experiment")
