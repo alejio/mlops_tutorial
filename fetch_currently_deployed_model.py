ca839734c8cb8fbecee82b41f41c17c062c48101
Alex Spanos <ge02040@hotmail.com>
2020-10-17 12:23:59 +0100

    interim commit

--- fetch_currently_deployed_model.py
+++ fetch_currently_deployed_model.py
@@ -8,8 +8,8 @@ from utils import get_mlflow_run
 logging.basicConfig(level=Config.LOGGING)
 
 
-def fetch_live_model_id():
-    run_id = get_mlflow_run(Config.TRACKING_URI, Config.EXPERIMENT_ID, Config.LIVE_TAG)
+def fetch_live_model_id(tracking_uri, experiment_id):
+    run_id = get_mlflow_run(tracking_uri, experiment_id, Config.LIVE_TAG)
     logging.info(f"Live model run_id is: {run_id}")
     print(run_id)
 
