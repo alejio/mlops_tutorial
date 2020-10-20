c4c77ecf3977fe315caa4245cced05ba2445c04a
Alex Spanos <ge02040@hotmail.com>
2020-10-20 09:40:20 +0100

    expreriment_id

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     # Your config! Go ahead and edit with your own values.
 
     USER = "alejio"  # UPDATE THIS WITH YOUR GITHUB HANDLE IN THE AlmostOps STAGE OF THE WORKSHOP
-    EXPERIMENT_ID = "6"  # update with what mlflow_setup.py returns
+    EXPERIMENT_ID = "2"  # update with what mlflow_setup.py returns
 
     # !!Leave the below variables as they are!!
     EXPERIMENT_NAME = f"{USER}_predict_sentiment"
@@ -17,7 +17,7 @@ class Config:
     CANDIDATE_TAG = "production_candidate"
     LOGGING = logging.INFO
     S3_DATA_DIR = "data"
-    S3_ARTIFACTS_DIR = f"artifacts/{USER}"
+    S3_ARTIFACTS_DIR = f"almostops_artifacts/{USER}"
     TRAIN_CSV = "train.csv"
     TEST_CSV = "test.csv"
     FEATURE_ENGINEERING_ARTIFACT = "feature_engineering.joblib"
